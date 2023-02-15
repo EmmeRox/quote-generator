f30360467b03c1fd4f5df7a3635d9913296ad7e7
EmmeRox <99152085+EmmeRox@users.noreply.github.com>
2023-02-15 13:58:07 -0600

    Manipulating the DOM to display data

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-// const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
-// Get quotes from API
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
 
 let apiQuotes = [];
 
@@ -7,7 +10,19 @@ let apiQuotes = [];
 function newQuote() {
   //Pick a random quote
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  console.log(quote);
+  // Check if author field is blank
+  if (!quote.author) {
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+  // Check  quote length
+  if (quote.text.length > 50) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  quoteText.textContent = quote.text;
 }
 
 async function getQuotes() {
